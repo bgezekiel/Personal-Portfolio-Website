8bf3420c8740912fd3ab2fef8d78d8b2a5a059d0
bgezekiel <44778576+bgezekiel@users.noreply.github.com>
2019-02-28 14:02:04 -0700

    update

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ $(window).on('scroll', function() {
 })
 
 
+$( document ).ready(function() {
+    $('#js-navbar-toggle').click(function() {
+        $('#js-menu').addClass('active')
+      });
+});
+
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
--- script2.js
+++ script2.js
@@ -4,6 +4,7 @@ function newFunction() {
     let mainNav = document.getElementById('js-menu');
     let navBarToggle = document.getElementById('js-navbar-toggle');
     navBarToggle.addEventListener('click', function () {
+        console.log("Here")
         mainNav.classList.toggle('active');
     });
 }
