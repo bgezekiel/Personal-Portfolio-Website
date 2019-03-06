ff4cfc741740546285e7fd1a4df504ec087a2bb7
Brandon Ezekiel <807777@ACDM.DS.SAIT.CA>
2019-03-06 15:27:24 -0700

    .

--- script.js
+++ script.js
@@ -14,8 +14,15 @@ $( document ).ready(function() {
       });
 });
 
-$( document ).ready(function() {
-    $('#js-menu').on('click', function() {
-        $('#js-menu').removeClass('active');
-        });
+jQuery('.scroll_to').click(function(e){
+    
+        var jump = $(this).attr('about');
+    
+        var new_position = $(jump).offset();
+    
+        $('html, body').stop().animate({ scrollTop: new_position.top }, 1000);
+    
+        e.preventDefault();
+    
     });
+    
